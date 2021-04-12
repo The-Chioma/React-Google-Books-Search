6323e912cf1d2ebd0b7d6ab6c1d6152b8e4d651d
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-04-12 17:13:17 +0100

    changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +18,10 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
+app.use(function (req, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+  
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
